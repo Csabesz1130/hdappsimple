628ecd7a9d7bb4308b288ff447abeca14a291487
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-01 19:08:05 +0200

    Add LocationView

---
+++ com/yourcompany/hdapp/views/LocationView.java
@@ -0,0 +1,14 @@
+package com.yourcompany.hdapp.views;
+
+import com.yourcompany.hdapp.MainFrame;
+
+import javax.swing.*;
+
+public class LocationView extends JPanel {
+    private MainFrame mainFrame;
+
+    public LocationView(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        // Implement the UI for location management
+    }
+}
