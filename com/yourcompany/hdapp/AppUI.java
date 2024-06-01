c23f2175e3cadfee19e8302c1fb4f4edfda7d0ac
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-01 19:02:36 +0200

    Add AppUI to initialize MainFrame

---
+++ com/yourcompany/hdapp/AppUI.java
@@ -0,0 +1,8 @@
+package com.yourcompany.hdapp;
+
+public class AppUI {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
