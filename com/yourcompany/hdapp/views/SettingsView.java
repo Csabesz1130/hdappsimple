12193f542b85fedecffe5254633b0d5bc4225193
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-01 19:08:15 +0200

    Add SettingsView

---
+++ com/yourcompany/hdapp/views/SettingsView.java
@@ -0,0 +1,14 @@
+package com.yourcompany.hdapp.views;
+
+import com.yourcompany.hdapp.MainFrame;
+
+import javax.swing.*;
+
+public class SettingsView extends JPanel {
+    private MainFrame mainFrame;
+
+    public SettingsView(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        // Implement the UI for settings management
+    }
+}
