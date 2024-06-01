fdc6fae3e20025b99504c9b07ed258d80ce7539b
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-01 19:08:48 +0200

    Add UserManagementView

---
+++ com/yourcompany/hdapp/views/UserManagementView.java
@@ -0,0 +1,14 @@
+package com.yourcompany.hdapp.views;
+
+import com.yourcompany.hdapp.MainFrame;
+
+import javax.swing.*;
+
+public class UserManagementView extends JPanel {
+    private MainFrame mainFrame;
+
+    public UserManagementView(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        // Implement the UI for user management
+    }
+}
