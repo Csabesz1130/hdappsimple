b505ecceacfed0147a6bf36573e96cc934bb3ff8
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-01 19:08:37 +0200

    Add NotificationsView

---
+++ com/yourcompany/hdapp/views/NotificationsView.java
@@ -0,0 +1,14 @@
+package com.yourcompany.hdapp.views;
+
+import com.yourcompany.hdapp.MainFrame;
+
+import javax.swing.*;
+
+public class NotificationsView extends JPanel {
+    private MainFrame mainFrame;
+
+    public NotificationsView(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        // Implement the UI for notifications management
+    }
+}
