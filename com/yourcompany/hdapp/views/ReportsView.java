3d641756d445b7212d46f1d623f22aaffc325557
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-01 19:08:27 +0200

    Add ReportsView

---
+++ com/yourcompany/hdapp/views/ReportsView.java
@@ -0,0 +1,14 @@
+package com.yourcompany.hdapp.views;
+
+import com.yourcompany.hdapp.MainFrame;
+
+import javax.swing.*;
+
+public class ReportsView extends JPanel {
+    private MainFrame mainFrame;
+
+    public ReportsView(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        // Implement the UI for reports management
+    }
+}
